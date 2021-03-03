4624508541149a9d653cc8c5e3b6beb2ad522efe
Tnaka-Kizuki <tnk_kzk5921@outlook.jp>
2021-03-03 20:15:24 +0900

    create Human class

---
+++ classes/Human.php
@@ -0,0 +1,26 @@
+<?php 
+
+class Human {
+
+     const MAX_HITPOINT = 100;
+     public $name;
+     public $hitPoint = 100;
+     public $attackPoint = 20;
+
+     public function doAttack($enemy) {
+          echo "[".$this->name."]の攻撃!\n";
+          echo "[".$enemy->name."]に".$this->attackPoint."のダメージ!";
+          $enemy->tookDamege($this->attackPoint);
+     }
+
+     public function tookDamage($damage) {
+          $this->point -= $damage;
+
+          if($this->hitPoint < 0) {
+               $this->hitPoint = 0;
+          }
+     }
+}
+
+
+?>
