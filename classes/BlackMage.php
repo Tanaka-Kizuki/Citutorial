e506f9bef471c010de707798fb70487b159b29ed
Tnaka-Kizuki <tnk_kzk5921@outlook.jp>
2021-03-04 00:00:56 +0900

    update main.php

--- classes/BlackMage.php
+++ classes/BlackMage.php
@@ -7,13 +7,18 @@ class BlackMage extends Human
     private $attackPoint = 10;
     private $intelligence = 30;
 
+    public function __construct($name)
+    {
+        parent::__construct($name, $this->hitPoint, $this->attackPoint);
+    }
+
     public function doAttack($enemy)
     {
         if (rand(1, 3) === 1) {
             echo "[" . $this->getName() . "]がスキルを発動した\n";
             echo "[ファイヤ]!!\n";
-            echo $enemy->getName."に".$this->intelligence*1.5."のダメージ!!\n";
-            $enemy->tookDamage($intelligence*1.5);
+            echo $enemy->getName()."に".$this->intelligence*1.5."のダメージ!!\n";
+            $enemy->tookDamage($this->intelligence*1.5);
         } else {
             parent::doAttack($enemy);
         }
--- main.php
+++ main.php
@@ -1,30 +1,98 @@
-<?php 
-
+<?php
+// ファイルのロード
 require_once('./classes/Human.php');
 require_once('./classes/Enemy.php');
 require_once('./classes/Brave.php');
+require_once('./classes/BlackMage.php');
+require_once('./classes/WhiteMage.php');
 
-$sawada = new Brave("さわだ");
-$goblin = new Enemy("ゴブリン");
+// インスタンス化
+$members = array();
+$members[] = new Brave('ティーダ');
+$members[] = new WhiteMage('ユウナ');
+$members[] = new BlackMage('ルールー');
+
+$enemies = array();
+$enemies[] = new Enemy('ゴブリン', 20);
+$enemies[] = new Enemy('ボム', 25);
+$enemies[] = new Enemy('モルボル', 30);
 
 $turn = 1;
+$isFinishFlg = false;
 
-while($sawada->getHitPoint() > 0 && $goblin->getHitPoint() > 0){
-     echo "**".$turn."ターン目**\n";
-     // ステータス
-     echo $sawada->getName() .":".$sawada->getHitPoint()."/".$sawada::MAX_HITPOINT."\n";
-     echo $goblin->getName() .":".$goblin->getHitPoint()."/".$goblin::MAX_HITPOINT."\n";
+while (!$isFinishFlg) {
+    echo "*** $turn ターン目 ***\n\n";
 
-     // 攻撃
-     $sawada->doAttack($goblin);
-     echo "\n";
-     $goblin->doAttack($sawada);
-     echo "\n";
+     // 現在のHPの表示
+    foreach ($members as $member) {
+        echo $member->getName() . "　：　" . $member->getHitPoint() . "/" . $member::MAX_HITPOINT . "\n";
+    }
+    echo "\n";
+    foreach ($enemies as $enemy) {
+        echo $enemy->getName() . "　：　" . $enemy->getHitPoint() . "/" . $enemy::MAX_HITPOINT . "\n";
+    }
+    echo "\n";
 
-     $turn++;
+    // 攻撃
+    foreach ($members as $member) {
+        $enemyIndex = rand(0, count($enemies) - 1); // 添字は0から始まるので、-1する
+        $enemy = $enemies[$enemyIndex];
+        // 白魔道士の場合、味方のオブジェクトも渡す
+        if (get_class($member) == "WhiteMage") {
+            $attackResult = $member->doAttackWhiteMage($enemy, $member);
+        } else {
+            $attackResult = $member->doAttack($enemy);
+        }
+        echo "\n";
+    }
+    echo "\n";
+
+    foreach ($enemies as $enemy) {
+        $memberIndex = rand(0, count($members) - 1); // 添字は0から始まるので、-1する
+        $member = $members[$memberIndex];
+        $enemy->doAttack($member);
+        echo "\n";
+    }
+    echo "\n";
+
+    // 仲間全員か敵全員のHPが０になるまで繰り返す
+    $deathCnt = 0; // HPが0以下の仲間の数
+    foreach ($members as $member) {
+        if ($member->getHitPoint() > 0) {
+            $isFinishFlg = false;
+            break;
+        }
+        $deathCnt++;
+    }
+    if ($deathCnt === count($members)) {
+        $isFinishFlg = true;
+        echo "GAME OVER ....\n\n";
+        break;
+    }
+
+    $deathCnt = 0; // HPが0以下の敵の数
+    foreach ($enemies as $enemy) {
+        if ($enemy->getHitPoint() > 0) {
+            $isFinishFlg = false;
+            break;
+        }
+        $deathCnt++;
+    }
+    if ($deathCnt === count($enemies)) {
+        $isFinishFlg = true;
+        echo "♪♪♪ファンファーレ♪♪♪\n\n";
+        break;
+    }
+    $turn++;
+
 }
-
 echo "★★★ 戦闘終了 ★★★\n\n";
-echo $sawada->getName() . "　：　" . $sawada->getHitPoint() . "/" . $sawada::MAX_HITPOINT . "\n";
-echo $goblin->getName() . "　：　" . $goblin->getHitPoint() . "/" . $goblin::MAX_HITPOINT . "\n\n";
-?>
+
+// 現在のHPの表示
+foreach ($members as $member) {
+    echo $member->getName() . "　：　" . $member->getHitPoint() . "/" . $member::MAX_HITPOINT . "\n";
+}
+echo "\n";
+foreach ($enemies as $enemy) {
+    echo $enemy->getName() . "　：　" . $enemy->getHitPoint() . "/" . $enemy::MAX_HITPOINT . "\n";
+}
