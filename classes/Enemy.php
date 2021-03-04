876ea4d8ea9574ca47d00c8e234b16dd7ba9a73e
Tnaka-Kizuki <tnk_kzk5921@outlook.jp>
2021-03-05 00:08:46 +0900

    update Enemy class

--- classes/Enemy.php
+++ classes/Enemy.php
@@ -1,60 +1,14 @@
 <?php
-class Enemy
+class Enemy extends Lives
 {
     const MAX_HITPOINT = 50; // 最大HPの定義 定数
-    private $name; // 敵の名前
-    private $hitPoint = 50; // 現在のHP
-    private $attackPoint = 10; // 攻撃力
 
     public function __construct($name, $attackPoint)
     {
-        $this->name = $name;
-        $this->attackPoint = $attackPoint;
+        $hitPoint = 50;
+        $intelligence = 0;
+        parent::__construct($name, $hitPoint, $attackPoint, $intelligence);
     }
-
-    // public function doAttack($human)
-    public function doAttack($humans) // ここを編集
-    {
-        //========== ここから追加する ==========
-        // チェック１：自身のHPが0かどうか
-        if ($this->hitPoint <= 0) {
-            return false;
-        }
-
-        $humanIndex = rand(0, count($humans) - 1); // 添字は0から始まるので、-1する
-        $human = $humans[$humanIndex];
-
-        //========== ここまで追加する ==========
-
-        echo "『" .$this->name . "』の攻撃！\n";
-        echo "【" . $human->getName() . "】に " . $this->attackPoint . " のダメージ！\n";
-        $human->tookDamage($this->attackPoint);
-    }
-
-    public function tookDamage($damage)
-    {
-        $this->hitPoint -= $damage;
-        // HPが0未満にならないための処理
-        if ($this->hitPoint < 0) {
-            $this->hitPoint = 0;
-        }
-    }
-
-    public function getName()
-    {
-        return $this->name;
-    }
-
-    public function getHitPoint()
-    {
-        return $this->hitPoint;
-    }
-
-    public function getAttackPoint()
-    {
-        return $this->attackPoint;
-    }
-
 }
 
 ?>
