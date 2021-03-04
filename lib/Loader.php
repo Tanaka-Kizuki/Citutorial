764e080ae530bec0dfec4e55d90d5891668e1edf
Tnaka-Kizuki <tnk_kzk5921@outlook.jp>
2021-03-05 00:48:24 +0900

    ureate Loader class

---
+++ lib/Loader.php
@@ -0,0 +1,28 @@
+<?php
+class Loader
+{
+     private $directories = array();
+
+     public function regDirectory($dir) {
+          $this->directories[] = $dir;
+          return;
+     }
+
+     public function register() {
+          spl_autoload_register(array($this,'loadClass'));
+     }
+
+     public function loadClass($className) {
+           // パスを１個ずつ取り出す
+        foreach ($this->directories as $dir) {
+          // クラスファイルへのパスを生成
+          $filePath = $dir . '/' . $className . '.php';
+          // $filePathが読み込めるかどうかチェック
+          if (is_readable($filePath)) {
+              // 読み込めたら、requireして、ループ終了
+              require $filePath;
+              return;
+          }
+      }
+     }
+}
